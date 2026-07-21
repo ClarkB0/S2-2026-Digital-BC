cc4c7d5ba9b44734f7e068d11c47fe6015812814
ClarkB0 <clarkb@pedarecc.sa.edu.au>
2026-07-21 14:10:59 +0930

    First exercise

--- starter.py
+++ starter.py
@@ -2,3 +2,14 @@
 raw_phrase = "aP!pL3e#S4aU%cE"
 
 # YOUR GOAL: Clean up this phrase using the 3 Security Rules below!
+def loop_solution(raw_phrase):
+    allowed_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    raw_phrase = raw_phrase.upper()
+    clean_phrase = ''
+    for char in raw_phrase:
+        if char in allowed_characters:
+            clean_phrase += char
+    print(clean_phrase)
+
+print(''.join(filter(str.isalpha, raw_phrase)).upper())
