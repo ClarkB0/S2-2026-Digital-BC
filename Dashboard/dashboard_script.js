57add4f38f62b359c2a4fac7a4e2086805adcbef
ClarkB0 <clarkb@pedarecc.sa.edu.au>
2026-09-09 20:08:33 +0930

    Fixed meter function for any range

--- Dashboard/dashboard_script.js
+++ Dashboard/dashboard_script.js
@@ -4,8 +4,9 @@ let aquariumData = null;
 let lastUpdated = "";
 
 // format these as the correct arrays later
-const temperatureRanges = [15, 20, 22, 26, 28, 35]
-const phRanges = [6, 6.5, 6.8, 7.8, 8.2, 9]
+const temperatureRanges = [[15, 20], [20, 22], [22, 26], [26, 28], [28, 35]];
+const phRanges = [[6, 6.5], [6.5, 6.8], [6.8, 7.8], [7.8, 8.2], [8.2, 9]];
+const ammoniaRanges = [[0, 0], [0, 0], [0, 0.02], [0.02, 0.05], [0.05, 0.06]]
 
 
 function loadData() {
@@ -48,23 +49,27 @@ function draw() {
     fill("gray");
     textSize(16);
     textAlign(RIGHT, TOP);
-    text("Last updated: " + (lastUpdated || "Loading..."), windowWidth - 15, 15);
+    text("Last updated: " + lastUpdated, windowWidth - 15, 15);
 
-    const ranges = [[0, 20], [20, 40], [40, 60], [60, 80], [80, 100]]
+    const temperature = aquariumData[0].exps.temperature.curr;
+    const ph = aquariumData[0].exps.ph.curr;
+    const ammonia = aquariumData[0].exps.nh3.curr;
 
-    drawMeter([100, 150], [0, 100], 10, 43, ranges)
+    drawMeter([100, 150], [temperatureRanges[0][0], temperatureRanges[4][1]], 2.5, temperature, temperatureRanges);
+    drawMeter([300, 150], [phRanges[0][0], phRanges[4][1]], 1, ph, phRanges);
+    drawMeter([500, 150], [ammoniaRanges[0][0], ammoniaRanges[4][1]], 0.01, ammonia, ammoniaRanges);
 }
 
 
 function drawMeter(position, extrema, interval, value, ranges=null) {
     const range = extrema[1] - extrema[0];
     const spacing = 200 / range;
-    const valueY = position[1] + 200 - value * spacing;
+    const valueY = position[1] + 200 - value * spacing + extrema[0] * spacing;
 
     if (ranges) {
         const colours = ["orange", "yellow", "chartreuse", "yellow", "orange"]
         for (let i = 0; i < 5; i++) {
-            const rangeY = position[1] + 200 - ranges[i][1] * spacing;
+            const rangeY = position[1] + 200 - ranges[i][1] * spacing + extrema[0] * spacing;
             const rangeHeight = (ranges[i][1] - ranges[i][0]) * spacing;
             fill(colours[i]);
             rect(position[0], rangeY, 50, rangeHeight);
@@ -78,11 +83,11 @@ function drawMeter(position, extrema, interval, value, ranges=null) {
 
     let label = extrema[0];
     noStroke();
-    while (label <= extrema[1]) {
+    while (label < extrema[1] + interval) {
         fill(0);
         textAlign(RIGHT, CENTER);
         textSize(10);
-        text(label, position[0] - 4, position[1] + 200 - label * spacing);
+        text(+label.toPrecision(4), position[0] - 4, position[1] + 200 - label * spacing + extrema[0] * spacing);
         label += interval;
     }
     stroke("red");
